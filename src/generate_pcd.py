b5fb11355bd5ca15eac6c2355c6f4d1f6b0547c5
Hanzhi Chen <ga84naj@mytum.de>
2020-06-07 16:35:44 +0800

    add inv depth func

--- src/generate_pcd.py
+++ src/generate_pcd.py
@@ -8,10 +8,13 @@ import string
 
 # im1 = Image.open(r'assets/depth.png')
 # im1.save(r'assets/depth2.png')
+def generate_inv_depth(depth_img):
+    inv_depth = cv2.bitwise_not(depth_img)
+    return inv_depth
+
 def generate_pcd(color_img, depth_img, intrinsics):
     rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_img, depth_img, convert_rgb_to_intensity=False)
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, intrinsics)
-
     pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
     return pcd
 
@@ -26,9 +29,12 @@ def get_intrinsics(file_path):
 
 
 if __name__ == "__main__":
+    depth_img = cv2.imread("assets/depth2.png")
+    inv_depth = generate_inv_depth(depth_img)
+    cv2.imwrite("assets/depth_inv.png", inv_depth)
 
     color_img = o3d.io.read_image("assets/rgb.png")
-    depth_img = o3d.io.read_image("assets/depth2.png")
+    depth_img = o3d.io.read_image("assets/depth_inv.png")
     intrinsics_list = get_intrinsics("assets/testintri.txt")
 
     fx = intrinsics_list[0]
@@ -39,7 +45,7 @@ if __name__ == "__main__":
     height = 512
 
     intrinsics = o3d.camera.PinholeCameraIntrinsic()
-    intrinsics.set_intrinsics(width=width, height=height, fx=fx, fy=fy, cx=cx,cy=cy)
+    intrinsics.set_intrinsics(width=width, height=height, fx=fx, fy=fy, cx=cx, cy=cy)
 
     pcd = generate_pcd(color_img, depth_img, intrinsics)
     o3d.io.write_point_cloud("assets/test.ply", pcd)
