5348c99a950e0728c15ebef3918c6246ea12e0d0
Hanzhi Chen <hanzhichen777@163.com>
2020-07-09 00:14:51 +0200

    bug fixed end2end rendering

--- ar_pipeline/ar_pipeline.py
+++ ar_pipeline/ar_pipeline.py
@@ -11,21 +11,25 @@ def clear_material( material ):
         material.node_tree.links.clear()
         material.node_tree.nodes.clear()
 
-materials = bpy.data.materials
+
 
 def create_shadowcatecher(name):
     mat_name = name
-
+    materials = bpy.data.materials
+    if materials.get( mat_name ) is not None:
+        return
+    
     material = materials.new( mat_name )
-
     if not material:
         material = materials.new( mat_name )
 
-    # We clear it as we'll define it completely
-    clear_material( material )
-
     material.use_nodes = True
+    clear_material( material )
+    for node in material.node_tree.nodes:
+#        if node.type != 'OUTPUT_MATERIAL': # skip the material output node as we'll need it later
+        nodes.remove(node) 
 
+    
     nodes = material.node_tree.nodes
     links = material.node_tree.links
 
@@ -48,7 +52,9 @@ def create_shadowcatecher(name):
     # Some setting for nodes
 
     diffuse2.inputs[0].default_value = (0,0,0,1)
-
+    colorramp.color_ramp.elements[0].color = (0,0,0,1)
+    colorramp.color_ramp.elements[1].position = (0.5)
+    colorramp.color_ramp.elements[1].color = (1, 1, 1, 1)
 
     #With names
     link1 = links.new( diffuse1.outputs['BSDF'], s2rgb.inputs['Shader'] )
@@ -82,9 +88,7 @@ def assign_material(obj, materialname):
 #    obj.data.materials[0] = bpy.data.materials[materialname]
     obj.data.materials.append(bpy.data.materials[materialname])
 #    if bpy.data.materials[materialname].use_transparency:
-#        obj.show_transparent = True
-
-
+#        obj.show_transparent = True   
 
 def create_compositor(img_path):
     # clear default nodes
@@ -100,8 +104,12 @@ def create_compositor(img_path):
     converter = tree.nodes.new(type="CompositorNodeAlphaOver")
 
     comp_node = tree.nodes.new('CompositorNodeComposite')   
+    
+    bpy.ops.image.open(filepath=img_path, show_multiview=False)
+    img_name = img_path.split("/")[-1]
+    
+    image_node.image = bpy.data.images[img_name]
 
-    image_node.image = bpy.data.images[img_path]
     image_node.location = 0,0
     comp_node.location = 400,0
 
@@ -111,60 +119,70 @@ def create_compositor(img_path):
     link2 = links.new(layer_node.outputs[0], converter.inputs[2])
     link3 = links.new(converter.outputs[0], comp_node.inputs[0])
 
-def prepare_camera(focal):
-    focal = 21
-    scene = bpy.context.collection
 
-    cam_data = bpy.data.cameras.new('camera')
+def prepare_camera(focal=21):
+    scene = bpy.context.collection
+    if bpy.data.objects.get("Camera") is not None:
+        bpy.data.objects.remove(bpy.data.objects["Camera"],do_unlink=True)
+    cam_data = bpy.data.cameras.new('Camera')
     cam_data.lens = focal
-    cam = bpy.data.objects.new('camera', cam_data)
+    cam = bpy.data.objects.new('Camera', cam_data)
     scene.objects.link(cam)
     bpy.context.scene.camera = cam
 
-    cam.location.x = 0
-    cam.location.y = 0
-    cam.location.z = 0
+    cam.location = (0, 0, 0)
 
     cam.rotation_mode = 'XYZ'
-    cam.rotation_euler[0] = 0
-    cam.rotation_euler[1] = 3.1416
-    cam.rotation_euler[2] = 3.1416
+    cam.rotation_euler = (0, 3.1416, 3.1416)
     
-def set_sun_light(position, rotation, energy, color=(1,1,1)):
+
+def prepare_sun_light(position, rotation, energy=8, color=(1,1,1)):
+#    bpy.data.objects.remove(bpy.data.objects["Light"],do_unlink=True)
+    if bpy.data.objects.get("Sun") is not None:
+        bpy.data.objects.remove(bpy.data.objects["Sun"],do_unlink=True)
+        
     bpy.ops.object.light_add(type="SUN")
     light_ob = bpy.context.object
     light_ob.location = position
-    light_ob.rotation_euler = rotation
+    light_ob.rotation_euler = (rotation[0]*3.1416/180, rotation[1]*3.1416/180, rotation[2]*3.1416/180)
     light = light_ob.data
     light.energy = energy
     light.color = color
     
+
 def render(resolution_x, resolution_y, path):
     bpy.context.scene.render.resolution_x = resolution_x
     bpy.context.scene.render.resolution_y = resolution_y
     bpy.context.scene.render.filepath = path
+    bpy.context.scene.render.film_transparent = True
     bpy.ops.render.render(write_still = True)
 
 
-def place_object(location, rotation, path):
+def prepare_ground(location=(0,0,0), rotation= (3.1416/2,0,0), size=30):
+#    objs = bpy.data.objects
+    if bpy.data.objects.get("Plane") is not None:
+        bpy.data.objects.remove(bpy.data.objects["Plane"],do_unlink=True)
+
+    ground = bpy.ops.mesh.primitive_plane_add(size=size, enter_editmode=False, align='WORLD', location=location, rotation=rotation)
+
+def place_object(location, rotation, obj_path):
     # TODO: set location and rotation of the object we want to place, then it will place the object at that pose.
     pass
-
-def create_catcherplane(location, rotation):
-    # TODO: after placing the object, we will need to know the bottom location of that object, so that we can place a plane under
-    # it for shadow projection.
-    pass 
 
 def import_scene(path):
     # TODO: import 3D scene, I think it might be better to call pcl_visualizer 
     pass
+
+def main():
+    prepare_ground(location=(0,0.15,0.0))
+    prepare_camera(21)
+    prepare_sun_light((0,-1.8,0),(173,41.4,127),10)
+    create_shadowcatecher("shadow_catcher")
+    create_compositor("C://Users//Lenovo//Documents//city.png")
+    catcher = bpy.data.objects['Plane']
+    assign_material(catcher, "shadow_catcher")
+    path = 'C://Users//Lenovo//Desktop//img.jpg'
+    render(1241, 376, path)
     
-
-
-set_sun_light((0,-1.211,0),(2.9844,0.6876,2.2165),8)
-catcher = bpy.data.objects['Plane']
-assign_material(catcher, "shadow_catcher")
-create_compositor('city.png')
-prepare_camera(21)
-path = 'C://Users//Lenovo//Desktop//img.jpg'
-render(1241, 376, path)
+if __name__ == '__main__':
+    main()
